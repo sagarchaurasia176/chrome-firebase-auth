b88f43e58b35185ccbf57fc9526ed2f962bdb90d
sagarchaurasia176 <101509099+sagarchaurasia176@users.noreply.github.com>
2025-03-09 17:55:53 +0530

    fetal:firebase

---
+++ public/signInWithPopup.js
@@ -0,0 +1,46 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
+import {
+  getAuth,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "https://www.gstatic.com/firebasejs/11.0.2/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDVmhrdeQ1ywcZzWdVsuiXIypymPHkaMWc",
+  authDomain: "code-analyzer-8dfab.firebaseapp.com",
+  projectId: "code-analyzer-8dfab",
+  storageBucket: "code-analyzer-8dfab.firebasestorage.app",
+  messagingSenderId: "956753356590",
+  appId: "1:956753356590:web:733a3db452a5e48c36fcbf",
+  measurementId: "G-BKRFWQEBZ1",
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+const PROVIDER = new GoogleAuthProvider();
+
+// This gives you a reference to the parent frame, i.e. the offscreen document.
+const PARENT_FRAME = document.location.ancestorOrigins[0];
+
+function sendResponse(result) {
+  window.parent.postMessage(JSON.stringify(result), PARENT_FRAME);
+}
+
+window.addEventListener("message", async function ({ data }) {
+  if (data.initAuth) {
+    try {
+      const result = await signInWithPopup(auth, PROVIDER);
+      const user = result.user;
+
+      if (user) {
+        // Get the ID token
+        const token = await user.getIdToken();
+
+        // Send token to parent frame
+        sendResponse({ token, user });
+      }
+    } catch (error) {
+      sendResponse({ error: error.message });
+    }
+  }
+});
